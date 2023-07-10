f39b10ee4d9c78afdb7161a6650f5f57f7936f6e
VyacheslavSaniuk <saniuk.vyacheslav.97@gmail.com>
2023-07-10 14:34:16 +0300

    Added JavaDocs to PropertiesFileService#read/readValue methods.

--- src/main/java/me/saniukvyacheslav/core/services/PropertiesService.java
+++ src/main/java/me/saniukvyacheslav/core/services/PropertiesService.java
@@ -8,8 +8,22 @@ import java.io.IOException;
 
 public interface PropertiesService {
 
+    /**
+     * Read property instance from repository by property key.
+     * @param aPropertyKey - property key.
+     * @return - read property instance.
+     * @throws PropertyNotFoundException - if property is not exist in repository.
+     * @throws IOException - if property is not exist in repository.
+     */
     Property read(String aPropertyKey) throws PropertyNotFoundException, IOException;
 
+    /**
+     * Read property value from repository by property key.
+     * @param aPropertyKey - property key.
+     * @return - property value.
+     * @throws PropertyNotFoundException - if property is not exist in repository.
+     * @throws IOException - if property is not exist in repository.
+     */
     String readValue(String aPropertyKey) throws PropertyNotFoundException, IOException;
 
     /*
