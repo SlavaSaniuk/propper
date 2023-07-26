85ba4dfc80ccd8b47731d306b80e119daef76f78
VyacheslavSaniuk <saniuk.vyacheslav.97@gmail.com>
2023-07-26 16:16:24 +0300

    Added JavaDocs.

--- propper-gui/src/main/java/me/saniukvyacheslav/gui/models/PropertyModel.java
+++ propper-gui/src/main/java/me/saniukvyacheslav/gui/models/PropertyModel.java
@@ -3,8 +3,6 @@ package me.saniukvyacheslav.gui.models;
 import lombok.Getter;
 import me.saniukvyacheslav.gui.controllers.props.PropertyChangesController;
 import me.saniukvyacheslav.gui.controllers.props.PropertyEvents;
-import me.saniukvyacheslav.gui.events.Observer;
-import me.saniukvyacheslav.gui.events.PropperApplicationEvent;
 import me.saniukvyacheslav.gui.nodes.PropertyField;
 import me.saniukvyacheslav.gui.views.PropertyView;
 import me.saniukvyacheslav.prop.Property;
@@ -14,7 +12,7 @@ import java.util.Objects;
 /**
  * Property model is a model for {@link Property} instance.
  */
-public class PropertyModel implements Observer {
+public class PropertyModel {
 
     // Class variables:
     @Getter private final Property property; // Origin property;
@@ -46,48 +44,8 @@ public class PropertyModel implements Observer {
         // Listener on changes of property value field:
         this.valuePropertyField.setOnKeyReleased(PropertyChangesController.getInstance().getKeyReleasedHandler());
 
-        // Subscribe this model on property changes controller:
-        PropertyChangesController.getInstance().subscribe(this, PropertyEvents.KEY_UPDATE_EVENT, PropertyEvents.VALUE_UPDATE_EVENT);
-    }
-
-    /**
-     * Update property model based on {@link PropertyEvents} events.
-     * This model listens {@link PropertyChangesController} controller on property changes events.
-     * Note: The first object in arguments array must be property key, to detect if changes link with current model instance.
-     * @param event - property changes event.
-     * @param arguments - array of arguments.
-     */
-    @Override
-    public void update(PropperApplicationEvent event, Object... arguments) {
-
-        // Check if this model instance was updating:
-        String keyOfUpdatedProperty = (String) arguments[0];
-        if (!(this.property.getPropertyKey().equals(keyOfUpdatedProperty)))
-            return;
-
-        switch (event.getCode()) {
-            case 201: // KEY_UPDATE_EVENT event:
-                this.onPropertyKeyUpdateEvent();
-                break;
-            case 202: // VALUE_UPDATE_EVENT event:
-                this.onPropertyValueUpdateEvent();
-                break;
-        }
-    }
-
-    /**
-     * Set "updated" css pseudo class to property fields.
-     * If property key was updated, that's why all property fields must have "updated" css pseudo class.
-     */
-    public void onPropertyKeyUpdateEvent() {
-        this.propertyView.setUpdatedWholeProperty(true);
-    }
-
-    /**
-     * Set "updated" css pseudo class to property value field.
-     */
-    public void onPropertyValueUpdateEvent() {
-        this.propertyView.setUpdatedPropertyValue(true);
+        // Subscribe property view on property changes controller:
+        PropertyChangesController.getInstance().subscribe(this.propertyView, PropertyEvents.KEY_UPDATE_EVENT, PropertyEvents.VALUE_UPDATE_EVENT);
     }
 
 }
--- propper-gui/src/main/java/me/saniukvyacheslav/gui/views/PropertyView.java
+++ propper-gui/src/main/java/me/saniukvyacheslav/gui/views/PropertyView.java
@@ -1,19 +1,35 @@
 package me.saniukvyacheslav.gui.views;
 
 
+import me.saniukvyacheslav.gui.controllers.props.PropertyChangesController;
+import me.saniukvyacheslav.gui.controllers.props.PropertyEvents;
+import me.saniukvyacheslav.gui.events.Observer;
+import me.saniukvyacheslav.gui.events.PropperApplicationEvent;
 import me.saniukvyacheslav.gui.models.PropertyModel;
 import me.saniukvyacheslav.gui.nodes.PropertyField;
 
-public class PropertyView implements Stylable {
+/**
+ * View for single {@link PropertyModel} model.
+ * This view implements {@link Observer} interface and it subscribed on {@link PropertyChangesController} controller.
+ */
+public class PropertyView implements Stylable, Observer {
 
-    private final PropertyField propertyKeyField;
-    private final PropertyField propertyValueField;
+    private final PropertyField propertyKeyField; // Property key text field;
+    private final PropertyField propertyValueField; // Property value text field;
 
+    /**
+     * Construct new view for specified {@link PropertyModel} model.
+     * @param aModel - property model.
+     */
     public PropertyView(PropertyModel aModel) {
         this.propertyKeyField = aModel.getKeyPropertyField();
         this.propertyValueField = aModel.getValuePropertyField();
     }
 
+    /**
+     * Stylize embedded property fields.
+     * Add "property-cell-common" css class.
+     */
     @Override
     public void stylize() {
 
@@ -26,10 +42,18 @@ public class PropertyView implements Stylable {
         );
     }
 
+    /**
+     * Set/unset "updated" css pseudo class to property key field.
+     * @param isUpdated - "updated" css pseudo class flag.
+     */
     public void setUpdatedPropertyKey(boolean isUpdated) {
         this.propertyKeyField.setUpdated(isUpdated);
     }
 
+    /**
+     * Set/unset "updated" css pseudo class to property value field.
+     * @param isUpdated - "updated" css pseudo class flag.
+     */
     public void setUpdatedPropertyValue(boolean isUpdated) {
         this.propertyValueField.setUpdated(isUpdated);
     }
@@ -41,5 +65,45 @@ public class PropertyView implements Stylable {
     public void setUpdatedWholeProperty(boolean isUpdated) {
         this.setUpdatedPropertyKey(isUpdated);
         this.setUpdatedPropertyValue(isUpdated);
+    }
+
+    /**
+     * Update property field based on {@link PropertyEvents} events.
+     * This view listens {@link PropertyChangesController} controller on property changes events.
+     * Note: The first object in arguments array must be property key, to detect if changes link with current view instance.
+     * @param event - property changes event.
+     * @param arguments - array of arguments.
+     */
+    @Override
+    public void update(PropperApplicationEvent event, Object... arguments) {
+
+        // Check if one of these fields was updating:
+        String keyOfUpdatedProperty = (String) arguments[0];
+        if (!(this.propertyKeyField.getPropertyKey().equals(keyOfUpdatedProperty)))
+            return;
+
+        switch (event.getCode()) {
+            case 201: // KEY_UPDATE_EVENT event:
+                this.onPropertyKeyUpdateEvent();
+                break;
+            case 202: // VALUE_UPDATE_EVENT event:
+                this.onPropertyValueUpdateEvent();
+                break;
+        }
+    }
+
+    /**
+     * Set "updated" css pseudo class to property fields.
+     * If property key was updated, that's why all property fields must have "updated" css pseudo class.
+     */
+    public void onPropertyKeyUpdateEvent() {
+        this.setUpdatedWholeProperty(true);
+    }
+
+    /**
+     * Set "updated" css pseudo class to property value field.
+     */
+    public void onPropertyValueUpdateEvent() {
+        this.setUpdatedPropertyValue(true);
     }
 }
