45d12ca279277dee12b39aef68fc5ef6380c9c86
VyacheslavSaniuk <saniuk.vyacheslav.97@gmail.com>
2023-08-16 10:25:10 +0300

    Added JavaDocs

--- propper-gui/src/main/java/me/saniukvyacheslav/gui/events/Observable.java
+++ propper-gui/src/main/java/me/saniukvyacheslav/gui/events/Observable.java
@@ -1,10 +1,27 @@
 package me.saniukvyacheslav.gui.events;
 
+/**
+ * Observable interface defines methods for subscribe, unsubscribe and updates {@link Observer} observers.
+ */
 public interface Observable {
 
+    /**
+     * Subscribe observer to this observable instance.
+     * @param anObserver - observer.
+     * @param anApplicationEvents - observer supported events.
+     */
     void subscribe(Observer anObserver, PropperApplicationEvent... anApplicationEvents);
 
+    /**
+     * Unsubscribe observer to this observable instance.
+     * @param anObserver - observer.
+     */
     void unsubscribe(Observer anObserver);
 
+    /**
+     * Notify observers of any event.
+     * @param anApplicationEvent - any event.
+     * @param anArguments - event arguments.
+     */
     void notify(PropperApplicationEvent anApplicationEvent, Object... anArguments);
 }
--- propper-gui/src/main/java/me/saniukvyacheslav/gui/events/Observer.java
+++ propper-gui/src/main/java/me/saniukvyacheslav/gui/events/Observer.java
@@ -1,6 +1,15 @@
 package me.saniukvyacheslav.gui.events;
 
+/**
+ * Observer interface used to specify that instance of class is observer and should be subscribed
+ * on {@link Observable) observable instances.
+ */
 public interface Observer {
 
+    /**
+     * Do something on event.
+     * @param event - {@link Observable} instance event.
+     * @param arguments - event arguments.
+     */
     void update(PropperApplicationEvent event, Object... arguments);
 }
